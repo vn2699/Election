0df205e97a829baa9919f84613ce2a52b0f981a7
Varun <vn2699@gmail.com>
2019-10-03 12:11:31 +0530

    My project

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,8 +7,14 @@ struct Candidate{
     string name;
     uint voteCount;
 }
+//store accounts that have voted
+mapping(address=>bool) public voters;
+//store Candidate
+//Fetch Candidate
 mapping(uint=>Candidate) public candidates;
+//store Candidate Count
 uint public candidatesCount;
+event votedEvent(uint indexed _candidateId);
 constructor () public{
     addCandidate("Candidate 1");
     addCandidate("Candidate 2");
@@ -16,5 +22,17 @@ constructor () public{
 function addCandidate (string memory _name) private{
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+}
+function vote(uint _candidateId) public {
+    //require that they haven't voted before
+    require(!voters[msg.sender]);
+    //require a valid candidate
+    require(_candidateId> 0 && _candidateId <= candidatesCount);
+    //record the voter has voted
+    voters[msg.sender]=true;
+    //Update Candidate vote Count
+    candidates[_candidateId].voteCount ++;
+
+    emit votedEvent(_candidateId);
 }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -26,11 +26,22 @@ App = {
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
-
+      App.listenForEvents();
       return App.render();
     });
   },
-
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
+    });
+  },  
   render: function() {
     var electionInstance;
     var loader = $("#loader");
@@ -55,6 +66,9 @@ App = {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
+      var candidatesSelect=$('#candidatesSelect');
+      candidatesSelect.empty();
+
       for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
@@ -64,13 +78,33 @@ App = {
           // Render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
+
+          //Render candidate ballot option
+          var candidateOption = "<option value='"+id+"'>" +name+"</option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted){
+      if(hasVoted){
+        $('form').hide();
+      }
       loader.hide();
       content.show();
     }).catch(function(error) {
       console.warn(error);
+    });
+  },
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
     });
   }
 };
